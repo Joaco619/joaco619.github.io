0db9f82b0f4f91ca386f7a313a11cf0ce6db2dda
Joaco619 <145710382+Joaco619@users.noreply.github.com>
2024-11-02 18:16:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const hostButton = document.getElementById('hostButton');
-const hostingURL = "URL_DEL_HOST"; // Reemplaza "URL_DEL_HOST" con la URL real
+const hostingURL = "joaco619.github.io"; // Reemplaza "URL_DEL_HOST" con la URL real
 
 hostButton.addEventListener('click', () => {
     window.location.href = hostingURL;
