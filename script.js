46b750effa459f17c903f15d76f8f74bde7465ac
Joaco619 <145710382+Joaco619@users.noreply.github.com>
2024-11-02 17:30:54 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const hostButton = document.getElementById('hostButton');
+
+hostButton.addEventListener('click', () => {
+    // Reemplaza "URL_DEL_HOST" con la URL real de tu página de hosting
+    window.location.href = "URL_DEL_HOST"; 
+});
